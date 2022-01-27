628c0d109d38d2129f3d090288682105e8f23d96
Joosep Parts <joosep.parts@gmail.com>
2022-01-27 09:56:24 +0200

    working socket connection

--- mindcuber/start_monitoring.py
+++ mindcuber/start_monitoring.py
@@ -1,14 +1,16 @@
 import time
 import websocket
-import _thread
+import asyncio
 import rel
 
 from data.HubMonitor import HubMonitor
 from comm.HubClient import HubClient, ConnectionState
 
-connection = 0
-
 rel.safe_read()
+connection = 0
+client = HubClient()
+hm = HubMonitor(client)
+client.start()
 
 
 def on_message(ws, message):
@@ -27,10 +29,34 @@ def on_open(ws):
     print("Opened connection")
 
 
-class Main:
+async def main():
+    hm.events.console_print += _console_print
 
-    if __name__ == "__main__":
-        websocket.enableTrace(True)
+
+def _console_print(msg):
+    print(msg)
+
+
+def send_heartbeat_to_backend(connection):
+    # TODO
+    if connection == 0:
+        print('HUB is offline')
+    elif connection == 1:
+        print('Connection established to HUB')
+    elif connection == 2:
+        if not hm.execution_status[1]:
+            print('Main program started on HUB')
+        if hm.execution_status[1]:
+            print('Main program running')
+
+
+def send_cube_map_to_backend(map):
+    # TODO
+    print(map)
+
+
+async def start_ws():
+    websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://localhost:8080/lobby",
                                 on_open=on_open,
                                 on_message=on_message,
@@ -41,46 +67,22 @@ class Main:
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
 
-    client = HubClient()
-    hm = HubMonitor(client)
 
-    def __init__(self):
-        self.hm.events.console_print += self._console_print
-        self.client.start()
-
-    def _console_print(self, msg):
-        print(msg)
-
-
-Main()
-
-
-def send_heartbeat_to_backend(connection):
-    # TODO
-    if connection == 0:
-        print('HUB is offline')
-    elif connection == 1:
-        print('Connection established to HUB')
-    elif connection == 2:
-        if not Main.hm.execution_status[1]:
-            print('Main program started on HUB')
-        # if Main.hm.execution_status[1]:
-    # print('Main program running')
-
-
-def send_cube_map_to_backend(map):
-    # TODO
-    print(map)
+def start_client():
+    asyncio.run(start_ws())
+    print("Finished")
 
 
 while True:
     send_heartbeat_to_backend(connection)
     time.sleep(0.1)
-    if Main.hm.connection_state == ConnectionState.TELEMETRY:
+    if hm.connection_state == ConnectionState.TELEMETRY:
         if connection == 0:
             connection = 1
         elif connection == 1:
-            Main.client.program_execute(1)
+            client.program_execute(1)
+            asyncio.run(main())
+            start_client()
             connection = 2
     else:
         print('waiting...')
